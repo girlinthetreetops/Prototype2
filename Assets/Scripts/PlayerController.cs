da184ea88bb1fd98769448541e68da849b238e41
girlinthetreetops <andrea.stykket@gmail.com>
2023-12-11 14:33:26 +0000

    Vertical movement added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
+
     public float speed = 10.0f;
     public float xRange = 10;
+    public float zRange = 5f;
 
     void Start()
     {
@@ -16,7 +19,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
 
         if (transform.position.x < -xRange)
@@ -28,5 +34,17 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+
+        if (transform.position.z < -zRange )
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
     }
 }
